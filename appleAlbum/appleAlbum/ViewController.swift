39ac231857a4e424a9c0b5bb21eefbacc2fa2515
WebberWong9797 <hkwebber1997@gmail.com>
2022-03-10 13:53:08 +0800

    first commit

---
+++ appleAlbum/appleAlbum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  appleAlbum
+//
+//  Created by Webber Wong on 10/3/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
